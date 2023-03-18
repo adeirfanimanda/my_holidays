5c2b96e98e3bc1229bf40ce3011c98065e4a5074
faiqshofwansyauqi <faiqshofwansyauqi@gmail.com>
2023-03-18 16:52:19 +0700

    profile and edit profile

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -30,7 +30,7 @@ class ProfilePage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Hello, Ade',
+                        'Hello, Ad',
                         style: primaryTextStyle.copyWith(
                           fontSize: 24,
                           fontWeight: semiBold,
