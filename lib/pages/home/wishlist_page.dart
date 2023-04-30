9776d2b03efbfadf66b911eb34ec965233c6b0fb
adeirfanimanda <adeheaven09@gmail.com>
2023-04-30 12:38:51 +0700

    menambahkan wishlist page empty

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -2,13 +2,88 @@ import 'package:flutter/material.dart';
 import 'package:my_holidays/theme.dart';
 
 class WishlistPage extends StatelessWidget {
+  const WishlistPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Wishlist Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: const Text(
+          'Favorite Tours',
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 74,
+              ),
+              const SizedBox(
+                height: 23,
+              ),
+              Text(
+                'You don\'t have dream tours?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Let\'s find your favorite tours',
+                style: secondaryTextStyle,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 24,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore Tours',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        emptyWishlist(),
+      ],
     );
   }
 }
